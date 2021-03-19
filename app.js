2177df8ff3e5a1fb8f9238b4b979087420507308
Dulat <astana7777777@gmail.com>
2021-03-19 03:21:53 -0700

    submission

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 function hoverEvent(event){
 
-
-
   var selectElement = event.target.childNodes;
 
   for (var i = 0; i < selectElement.length; i++) {
@@ -40,8 +38,10 @@ function addBox(event){
     for (var i = 0; i < element.childNodes.length; i++) {
         if (element.childNodes[i].className == "CommonRectangle") {
           var findElement = element.childNodes[i];
-          var newElem = '<div class="insideRectangleOne"></div>';
-          findElement.innerHTML += newElem;
+          var newElem = document.createElement("div")
+          newElem.className = "insideRectangleOne";
+          setColor(newElem);
+          findElement.appendChild(newElem);
         }
     }
 
@@ -49,15 +49,16 @@ function addBox(event){
     for (i = 0; i < select.length; i++) {
       select[i].style.display = "none";
     }
-}
 
+
+}
 
 
 function addRectangle(event){
 
     var CopyBlock = event.target.parentElement.parentNode.parentElement;
     var clonedElement = CopyBlock.cloneNode(true);
-    var insertBlock = '<div class="Rectangle"> <div class="CommonRectangle"> <!-- <div class="insideRectangleOne"></div> <div class="insideRectangleTwo"></div> --> </div> <div class="insideRectangleThree" onmouseover="hoverEvent(event)" > <div class="buttonText" onmouseover="hoverEventChild(event)" > Add </div> <div class="addBox"> <div class="addBoxIn LongPress" onmouseup="PressUpEvent()" onclick="addBox(event)"> <div class="buttonText"> Box </div> </div> <div class="addBoxInTwo LongPress" onclick="addRectangle(event)"> <div class="buttonText"> Container </div> </div> </div> </div> </div>';
+    var insertBlock = '<div class="Rectangle"> <div class="CommonRectangle"> </div> <div class="insideRectangleThree" onmouseover="hoverEvent(event)" > <div class="buttonText" onmouseover="hoverEventChild(event)" > Add </div> <div class="addBox"> <div class="addBoxIn LongPress" onmouseup="PressUpEvent()" onclick="addBox(event)"> <div class="buttonText"> Box </div> </div> <div class="addBoxInTwo LongPress" onclick="addRectangle(event)"> <div class="buttonText"> Container </div> </div> </div> </div> </div>';
 
     var insideCommonRectangle = event.target.parentElement.parentNode.parentElement.parentNode.firstElementChild;
     insideCommonRectangle.innerHTML += insertBlock;
@@ -69,14 +70,6 @@ function addRectangle(event){
 
 }
 
-// function addRectangle(event){
-//
-//     var CopyBlock = event.target.parentElement.parentNode.parentElement;
-//     //console.log(CopyBlock.className);
-//     var clonedElement = CopyBlock.cloneNode(true);
-//     var insideCommonRectangle = event.target.parentElement.parentNode.parentElement.parentNode.firstElementChild;
-//     insideCommonRectangle.appendChild(clonedElement);
-// }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -89,46 +82,3 @@ function setColor(element){
   selectedElement.style.backgroundColor = colorArray[getRandomInt(3)];
 
 }
-//long press event
-
-function PressUpEvent(){
-// Create variable for setTimeout
-      var delay;
-
-      // Set number of milliseconds for longpress
-      var longpress = 1300;
-
-      var listItems = document.getElementsByClassName('LongPress');
-      var listItem;
-
-      for (var i = 0, j = listItems.length; i < j; i++) {
-        listItem = listItems[i];
-
-        listItem.addEventListener('mousedown', function (e) {
-          var _this = this;
-          delay = setTimeout(check, longpress);
-
-          function check() {
-              _this.classList.add('is-selected');
-
-                setColor(_this);
-              console.log("selected");
-          }
-
-        }, true);
-
-        listItem.addEventListener('mouseup', function (e) {
-          // On mouse up, we know it is no longer a longpress
-          clearTimeout(delay);
-        });
-
-        listItem.addEventListener('mouseout', function (e) {
-          clearTimeout(delay);
-        });
-
-      }
-
-}
-
-PressUpEvent();
-//long press event
